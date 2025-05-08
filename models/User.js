82848823ace0ed2212d37f7b1ecff850dd6d0a6e
Sandeep Sinha <sandeepsinha1210@gmail.com>
2025-05-09 03:35:55 +0530

    Minor password change

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, select: false },
     phone: { type: String, required: true },
     address: { type: String, required: true },
     role: { type: Number, default: 0 },
